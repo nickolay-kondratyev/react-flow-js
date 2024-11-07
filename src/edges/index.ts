40abde24c30bdcad4e77e91fb4253ebac7017e1a
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2024-11-06 19:48:33 -0800

    Get the build to build

--- src/edges/index.ts
+++ src/edges/index.ts
@@ -4,8 +4,8 @@ export const initialEdges = [
   { id: "a->c", source: "a", target: "c", animated: true },
   { id: "b->d", source: "b", target: "d" },
   { id: "c->d", source: "c", target: "d", animated: true },
-] satisfies Edge[];
+];
 
 export const edgeTypes = {
   // Add your custom edge types here!
-} satisfies EdgeTypes;
+}
